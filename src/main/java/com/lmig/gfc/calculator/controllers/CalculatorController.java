007e52273569546c8f84cc5ad3a8eb2fadef40de
Gene Merryman <gene.merryman@libertymutual.com>
2017-11-10 10:22:36 -0500

    Calculator outputs results

--- src/main/java/com/lmig/gfc/calculator/controllers/CalculatorController.java
+++ src/main/java/com/lmig/gfc/calculator/controllers/CalculatorController.java
@@ -8,9 +8,9 @@ import com.lmig.gfc.calculator.models.Calculator;
 
 @Controller
 public class CalculatorController {
-	
+
 	private Calculator calc;
-	
+
 	@RequestMapping("/")
 	public ModelAndView showApp() {
 		ModelAndView mv = new ModelAndView();
@@ -18,18 +18,17 @@ public class CalculatorController {
 		return mv;
 	}
 
-
 	@RequestMapping("/math")
 	public ModelAndView doMath(int firstNumber, int secondNumber, String operator) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("calculator");
-		Calculator input = new Calculator(firstNumber, secondNumber, operator);
-		
+		Calculator calc; // declare the variable
+		calc = new Calculator(firstNumber, secondNumber, operator); // set the variable
+		mv.addObject("result", calc.doMath()); // object to the view.. value returned from doMath() method..
+		mv.addObject("firstNumber", firstNumber);
+		mv.addObject("secondNumber", secondNumber);
+		mv.addObject("operator", operator);
 		return mv;
 	}
-	
-	
-
-	
 
 }
--- src/main/java/com/lmig/gfc/calculator/models/Calculator.java
+++ src/main/java/com/lmig/gfc/calculator/models/Calculator.java
@@ -1,36 +1,48 @@
 package com.lmig.gfc.calculator.models;
 
 public class Calculator {
-	
+
 	private String operator;
 	private int firstNumber;
 	private int secondNumber;
-	private int results;
-	
+	private double results;
+
 	public Calculator(int firstNumber, int secondNumber, String operator) {
 		this.firstNumber = firstNumber;
 		this.secondNumber = secondNumber;
 		this.operator = operator;
 	}
-	
-	public int doMathAdd() {
-		results = firstNumber + secondNumber;
+
+	public double doMath() {
+		if (operator.equals("+")) {
+			results = firstNumber + secondNumber;
+		}
+		if (operator.equals("-")) {
+			results = firstNumber - secondNumber;
+		}
+		if (operator.equals("*")) {
+			results = firstNumber * secondNumber;
+		}
+		if (operator.equals("/")) {
+			results = firstNumber / secondNumber;
+		}
+		if (operator.equals("^")) {
+			results = Math.pow(firstNumber, secondNumber);
+		}
 		return results;
+
 	}
-	
+
 	public String getOperator() {
 		return operator;
 	}
+
 	public int getFirstNumber() {
 		return firstNumber;
 	}
+
 	public int getSecondNumber() {
 		return secondNumber;
 	}
-	
-	
 
-	
-
-	
 }
